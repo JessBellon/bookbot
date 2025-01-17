366a003be95c1b74df84be9e29cc22874af0d8bc
JessBellon <jess@jessicabellon.com>
2025-01-17 16:46:55 -0600

    L 9: break out into functions

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+def get_file_contents(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def get_word_count(contents): 
+    words = contents.split()
+    num = len(words)
+    return num
 def main(): 
     try:
-        with open("./books/frankenstein.txt") as f:
-            file_contents = f.read()
-            words = file_contents.split()
-            print(len(words))
+        contents = get_file_contents("./books/frankenstein.txt")
+        word_count = get_word_count(contents)
+        print(f"There are {word_count} words.")
     except Exception as e:
         print(f"error: {e}")
 
