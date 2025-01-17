32f66ea777594f468b38847612ad1d69b20fb1c5
JessBellon <jess@jessicabellon.com>
2025-01-17 16:42:42 -0600

    L 9: Count Words

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     try:
         with open("./books/frankenstein.txt") as f:
             file_contents = f.read()
-            print(file_contents)
+            words = file_contents.split()
+            print(len(words))
     except Exception as e:
         print(f"error: {e}")
 
