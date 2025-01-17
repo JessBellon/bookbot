5ac6f0d56ba269f825451fcbd2c7294c3ceab029
JessBellon <jess@jessicabellon.com>
2025-01-17 16:58:56 -0600

    L 10

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ def get_file_contents(path):
         file_contents = f.read()
     return file_contents
 
+def get_words_as_list(contents):
+    return contents.split()
+
 def get_word_count(contents): 
-    words = contents.split()
+    words = get_words_as_list(contents)
     num = len(words)
     return num
+
+def get_char_count_dict(contents):
+    letters = {}
+    for l in contents:
+        if l not in letters:
+            letters[l] = 0
+        letters[l] += 1
+    return letters
+
+
+
 def main(): 
     try:
+        # contents = get_file_contents("./books/debug.txt")
         contents = get_file_contents("./books/frankenstein.txt")
-        word_count = get_word_count(contents)
-        print(f"There are {word_count} words.")
+        # word_count = get_word_count(contents)
+        # print(f"There are {word_count} words.")
+        char_cts =get_char_count_dict(contents.lower())
+        print(char_cts)
     except Exception as e:
         print(f"error: {e}")
 
