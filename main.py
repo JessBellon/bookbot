6d6c7fc6be94704be1b20e175b454555e14930f8
JessBellon <jess@jessicabellon.com>
2025-01-17 18:01:46 -0600

    end of guided project

--- main.py
+++ main.py
@@ -19,16 +19,28 @@ def get_char_count_dict(contents):
         letters[l] += 1
     return letters
 
+def report_on_char_count(char_cts):
+    list_of_dicts = []
+    for k in char_cts:
+        if k.isalpha():
+            list_of_dicts.append({ "char": k , "count": char_cts[k] })
+    
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    for dic in list_of_dicts:
+        print(f"The '{dic["char"]}' character was found {dic["count"]} times")
+    
 
+
+def sort_on(dict):
+    return dict["count"]
 
 def main(): 
     try:
-        # contents = get_file_contents("./books/debug.txt")
         contents = get_file_contents("./books/frankenstein.txt")
-        # word_count = get_word_count(contents)
-        # print(f"There are {word_count} words.")
-        char_cts =get_char_count_dict(contents.lower())
-        print(char_cts)
+        word_count = get_word_count(contents)
+        print(f"There are {word_count} words.")
+        char_cts = get_char_count_dict(contents.lower())
+        report_on_char_count(char_cts)
     except Exception as e:
         print(f"error: {e}")
 
